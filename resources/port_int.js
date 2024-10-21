0755b6ba3c301bb81f401ae8074a4f43704d5aa3
LadislavS <sachr8@seznam.cz>
2024-10-21 22:48:34 +0200

    try interval

--- resources/port_int.js
+++ resources/port_int.js
@@ -12,7 +12,10 @@ adadiv.hidden=true;
 
 const startdate=new Date("October 20, 2024 01:19:00");
 const current=new Date();
-counter.innerHTML=`Fun fact! this page is live for ${Math.floor((current-startdate)/86400000)} days!` // 60000-minute ,86400000-day
+/*setinterval(()=>{
+    counter.innerHTML=`Fun fact! this page is live for ${Math.floor((current-startdate)/1000)} seconds!`;
+},1000);*/
+counter.innerHTML=`Fun fact! this page is live for ${Math.floor((current-startdate)/86400000)} days!` // 1000-seconds, 60000-minute ,86400000-day
 
 first.onclick=function(){
     if(firstdiv.hidden===true){
